c7661993370b7b4fde846fb8b0dbcb80f8e7b373
Sumit Kumar <Sumit.Kumar@belden.com>
2026-03-25 00:33:25 +0530

    refactor: Comment out FoundersSection in HomePage component
    This commit comments out the FoundersSection in the HomePage component to streamline the layout. The change aims to enhance the overall user experience by simplifying the displayed sections.

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
         <HiringPartnerReview />
         <StudentReviews />
         <MentorSection />
-        <FoundersSection />
+        {/* <FoundersSection /> */}
         <FAQSection />
         <ApplySection />
       </main>
